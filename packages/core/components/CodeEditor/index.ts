3b44e1acf01f6993d801f6783c2d52859f38cf34
thinkgem <thinkgem@163.com>
2026-08-21 09:18:04 +0800

    优化 CodeEditor 组件增加 loading

--- packages/core/components/CodeEditor/index.ts
+++ packages/core/components/CodeEditor/index.ts
@@ -1,6 +1,6 @@
 import { createAsyncComponent } from '@jeesite/core/utils/factory/createAsyncComponent';
 
 export * from './src/codemirror/typing';
-export const CodeEditor = createAsyncComponent(() => import('./src/codemirror/CodeEditor.vue'));
-export const JsonPreview = createAsyncComponent(() => import('./src/json-preview/JsonPreview.vue'));
-export const MonacoEditor = createAsyncComponent(() => import('./src/MonacoEditor/index.vue'));
+export const CodeEditor = createAsyncComponent(() => import('./src/codemirror/CodeEditor.vue'), { loading: true });
+export const JsonPreview = createAsyncComponent(() => import('./src/json-preview/JsonPreview.vue'), { loading: true });
+export const MonacoEditor = createAsyncComponent(() => import('./src/MonacoEditor/index.vue'), { loading: true });
